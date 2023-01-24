42096f433d3ad9d15a255ba52d64989d78680727
Alex Saavedra <fass28@icloud.com>
2023-01-24 15:54:52 -0500

    start section done

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,14 +4,15 @@ import avatarImg from "../assets/avataaars.svg"
 export const Start = () => {
   return (
     <>
-      <div className="container d-flex align-items-center flex-column">
+      <div className="container d-flex align-items-center flex-column start">
         <img src={avatarImg} alt="sadsad" />
-        <p>START BOOTSTRAP</p>
+        <h1>START BOOTSTRAP</h1>
         <span>
           <hr />
           <FontAwesomeIcon icon="fa-solid fa-star" /> 
           <hr />
         </span>
+        <h6>Graphic Artist - Web Designer - Illustrator</h6>
       </div>
     </>
   )
