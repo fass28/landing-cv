af7666f310320fb759783d2eb3e7dae5c7a4dd77
Alex Saavedra <fass28@icloud.com>
2023-01-24 21:44:33 -0500

    contact me section css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <section className="contact-me">
+      <form autocomplete="off">
+        <div className='contact-title'>
+          <h2>CONTACT ME</h2>
+          <span>
+              <hr />
+              <FontAwesomeIcon icon="fa-solid fa-star" /> 
+              <hr />
+          </span>
+        </div>
+        <div >
+          <input placeholder="Full name" type="text" />
+        </div>
+        <div >
+          <input placeholder="Email address" type="text"  />
+        </div>
+        <div >
+          <input placeholder="Phone number" type="text"  />
+        </div>
+        <div >
+          <textarea placeholder="Message" type="text"  />
+        </div>
+        <button className="btn btn-success">Send</button>
+      </form>
+    </section>
+    </>
+
+
+
+    
   )
 }
