8a68e3528fb3ee570d45d627fbd4b923cf2a4db5
Alex Saavedra <fass28@icloud.com>
2023-01-25 18:05:36 -0500

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className="footer  text-center">
+        <div className="row">
+          <ul className="col-4">
+            <h4>LOCATION</h4>
+            <h5>2215 John Daniel Drive Clark, MO 65243</h5>
+          </ul>
+          <ul className="col-4 ">
+            <h4>AROUND THE WEB</h4>
+            <a href="" id='icon'><FontAwesomeIcon icon={['fab', 'facebook']} className="icon" /> </a>
+            <a href="" id='icon'><FontAwesomeIcon icon={['fab', 'twitter']} className="icon"/> </a>
+            <a href="" id='icon'><FontAwesomeIcon icon={['fab', 'linkedin']}  className="icon"/> </a>
+            <a href="" id='icon'><FontAwesomeIcon icon={['fab', 'instagram']}  className="icon"/> </a>
+          </ul>
+          <ul className="col-4">
+            <h4>ABOUT FREELANCER</h4>
+            <h5>Freelance is a free to use, MIT licensed Bootstrap theme created by <a href="">Start Bootstrap.</a></h5>
+          </ul>
+        </div>
+        <div>
+
+        </div>
+
+      </section>
+        <div className='copyright'>
+          <h6>Copyright © Your Website 2022</h6>
+        </div>
+    </>
   )
 }
--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,4 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import {fab, faTwitterSquare, faFacebook, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
 
-library.add(fas)
+
+library.add(fas,faTwitterSquare, faFacebook, faLinkedin, faGithub,fab)
